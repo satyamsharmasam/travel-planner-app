f2bbbf24add136ec185d55fc47a39e950c28861d
Satyam sharma <sharmasam93500@gmail.com>
2025-09-02 11:35:25 +0530

    add button component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,26 +1,28 @@
+import Button from './ui/Button';
 import SplitText from './ui/welcomeText';
 
-const handleAnimationComplete = () => {
-  console.log('All letters have animated!');
-};
 const HeroSection = () => {
   return (
     <>
-      <div className='h-screen flex items-center justify-center '>
-        <SplitText
-          text='Journezy: Where AI guides your adventure!'
-          className={`text-5xl md:text-7xl font-bold text-center leading-snug md:leading-normal `}
-          delay={100}
-          duration={0.6}
-          ease='power3.out'
-          splitType='chars'
-          from={{ opacity: 0, y: 40 }}
-          to={{ opacity: 1, y: 0 }}
-          threshold={0.1}
-          rootMargin='-100px'
-          textAlign='center'
-          onLetterAnimationComplete={handleAnimationComplete}
-        />
+      <div>
+        <div className='h-[calc(100vh-20px)] flex items-center justify-center absolute '>
+          <SplitText
+            text='Journezy:Where AI guides your adventure!'
+            className={`text-5xl md:text-7xl font-bold text-center leading-snug md:leading-normal `}
+            delay={100}
+            duration={0.6}
+            ease='power3.out'
+            splitType='chars'
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin='-100px'
+            textAlign='center'
+          />
+          <div className='absolute top-[85%]'>
+            <Button />
+          </div>
+        </div>
       </div>
     </>
   );
